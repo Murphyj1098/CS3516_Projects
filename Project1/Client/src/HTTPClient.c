8534f3e13697c41f9f0a8b3d82ca6b1fd773e48d
Murphyj1098 <jrmurphy@wpi.edu>
2020-09-16 15:44:29 -0400

    Server connection and writing to server

--- Project1/Client/src/HTTPClient.c
+++ Project1/Client/src/HTTPClient.c
@@ -1,24 +1,28 @@
 // Joseph Murphy (jrmurphy)
 // CS3516 Project 1 - Socket Programming
 
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include "HTTPClient.h"
 
 int main(int argc, char** argv)
 {
-
-    char* rawUrl, formatUrl;
+    char* rawUrl;
     int socketID;
+    char* serverPort;
+
+    char rsp[800];
 
     struct timeval start, end;
-
+    struct addrinfo *serverAddress;
 
     // 3 or 4 possible arguments
     // ./http_client [-options] server_url port_number
@@ -28,6 +32,7 @@ int main(int argc, char** argv)
         exit(1);
     }
     
+    // check extra argument is "-p"
     if(argc == 4)
     {
         if(strcmp(argv[1], "-p")) // check if provided option is -p
@@ -36,41 +41,64 @@ int main(int argc, char** argv)
            usage();
            exit(1); 
         }
-        rawUrl = argv[2];
-    }
-    else if(argc == 3)
-    {
-        rawUrl = argv[1];
     }
     
-    // format url to appropriate format
+    // Copy url and port (position in array based on number of args)
+    rawUrl = (argc == 3) ? argv[1] : argv[2];
+    serverPort = (argc == 3) ? argv[2] : argv[3];
+    
+
     // get address info
+    if(getaddrinfo(rawUrl, serverPort, /*TODO: hints array?*/ 0, &serverAddress) < 0)
+    {
+        printf("Failed to get address info\n");
+        exit(1);
+    }
+    printf("Got address info\n");
 
     if((socketID = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("Failed to open socket\n");
-        exit(2);
+        exit(1);
     }
+    printf("Opened socket\n");
 
     // start RTT measurement
     gettimeofday(&start, NULL);
 
     // connect to socket
-    // if(connect(socketID, /*a pointer to a socket address structure*/x, /*the size of the socket address structure*//x) != 0)
+    if (connect(socketID, serverAddress->ai_addr, serverAddress->ai_addrlen) < 0)
     {
         printf("Failed to connect to server\n");
-        exit(3);
+        exit(1);
     }
+    printf("Connected to server\n");
+
+    gettimeofday(&end, NULL);
 
     // write GET request to server
-    
+    write(socketID, "GET ~yli15/courses/CS3516Fall20A//index.html HTTP1.1\r\n", 10000);
+    write(socketID, "Host: users.wpi.edu\r\n", 10000);
+    write(socketID, "Content-Type: text/html\r\n", 10000);
+    write(socketID, "Connection: close\r\n", 10000);
+    write(socketID, "\r\n", 10000);
 
     // receive response and store
-    // print RTT measure (if -p)
-    // close socket
-    // end program
+    printf("reading\n");
+    read(socketID, rsp, sizeof(rsp));
 
+    printf("%s", rsp);
+
+
+    // print RTT measure (if -p)
+
+
+    // close socket
+    close(socketID);
+    exit(0);
 }
+
+
 
 // print out program usage to console
 void usage()
